08c0023126e1a2b650c9715f4e5b297cd5169cb8
Santos Upadhyay <anynomouscoder0@gmail.com>
2024-01-03 08:42:20 +0545

    First Project

---
+++ Random Password Generator/script.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector('.gen-btn');
+const passwordBox = document.getElementById('password')
+const lengthPsw = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+const numbers = "0123456789"
+const specialChar = "!@#$%^&*()_+=-?/><"
+
+const allChars = upperCase + lowerCase + numbers + specialChar;
+
+btn.addEventListener('click', function(){
+    let password = "";
+
+    password += upperCase[Math.floor(Math.random * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random * lowerCase.length)];
+    password += numbers[Math.floor(Math.random * numbers.length)];
+    password += specialChar[Math.floor(Math.random * specialChar.length)];
+
+    while(lengthPsw > password.length){
+        password += allChars[Math.floor(Math.random * allChars.length)];
+    }
+
+    passwordBox.value = password;
+})
